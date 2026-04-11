cc04cfe3ed5be75f14a62f81d000486abd587d41
Alvin <yaoyu0906@gmail.com>
2026-04-11 11:45:40 +0800

    Added navigations
    Add navigation functions for beta, events, and profile

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function goBeta() {
+    window.location.href = "/beta";
+}
+
+function goEvents() {
+    window.location.href = "/beta/events";
+}
+
+function goProfile() {
+    window.location.href = "/profile";
+}
