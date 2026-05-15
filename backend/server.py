600905fd7e25571318404a81e31112c07de520c9
Alvin <yaoyu0906@gmail.com>
2026-05-15 16:27:32 +0800

    Redo
    Uh why was it wrong before :/

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import traceback
 import psycopg2
 import os
 
 app = Flask(__name__)
-CORS(app, origins="*", supports_credentials=True, allow_headers=["Content-Type"], methods=["GET", "POST", "OPTIONS"])
+CORS(app, origins="*")
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
@@ -685,7 +684,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
-try:
-    init_db()
-except Exception as e:
-    print("INIT ERROR:", traceback.format_exc())
+init_db()
